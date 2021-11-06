4b53d18841c7b1d16ce80ce9d5dd5d30c3c8a642
Ludwig Oleby <oleby.ludwig@gmail.com>
2021-11-06 23:52:21 +0100

    Added  return statements to functions and modified the main method so that the program will print the methods correctly according to the assignment.

--- OOP_labb1/Program.cs
+++ OOP_labb1/Program.cs
@@ -9,17 +9,17 @@ namespace OOP_labb1
         {
             Circle c1 = new Circle(5);
             Console.WriteLine("Cirkelns area:");
-            c1.getArea();
+            Console.WriteLine(c1.getArea());
             Console.WriteLine("Cirkelns omkrets:");
-            c1.getCircumference();
+            Console.WriteLine(c1.getCircumference());
 
             Console.WriteLine(" ");
 
             Circle c2 = new Circle(6);
             Console.WriteLine("Cirkelns area:");
-            c2.getArea();
+            Console.WriteLine(c2.getArea());
             Console.WriteLine("Cirkelns omkrets:");
-            c2.getCircumference();
+            Console.WriteLine(c2.getCircumference());
 
         }
     }
@@ -35,17 +35,18 @@ namespace OOP_labb1
             this._radie = r;
         }
 
-        public void getArea()
+        public double getArea()
         {
             float area = _radie * _radie * _pi;
-            Console.WriteLine(area);
+            return area;
+            
         }
 
-        public void getCircumference() //Extra utmaning omkrets
+        public double getCircumference() //Extra utmaning omkrets
         {
             float diameter = _radie * 2;
             float circumference = diameter * _pi;
-            Console.WriteLine(circumference);
+            return circumference;
         }
 
     }
